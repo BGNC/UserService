640af6408a348da5f36d3378734b4f7f66f7af4b
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-07-02 18:02:13 +0300

    log

--- src/main/java/com/bgnc/userservice/model/User.java
+++ src/main/java/com/bgnc/userservice/model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -13,7 +14,7 @@ import java.util.Collection;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
